2b833db405af62184a2d981182da051a35fae731
fahadansari89 <user.email like :"mohd936994@gmail.com>
2025-03-09 12:05:06 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 const corsOption={
-    origin:'http://localhost:5173',
+    origin:'https://job-portal-2-u0l1.onrender.com',
     credentials:true
 }
 app.use(cors(corsOption))
